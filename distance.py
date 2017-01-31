b75188372b87ac88aa6e5b0fd088d1f1e7541ea7
daniel90 <daniel.pecak@gmail.com>
2017-01-31 09:04:41 +0100

    Calculate distance between Countries

--- distance.py
+++ distance.py
@@ -1,4 +1,31 @@
 # -*- coding: utf-8 -*-
+import math
 import csv
 
-# with
+# Constants:
+R = 6371. # mean radius of the Earth that minimalizes the fact that the Earth
+          # is not spherical but elipsoidal
+
+# Load the CSV file with Countries and coordinates
+countries = []
+with open('south-america.csv','rb') as file:
+    reader = csv.reader(file,delimiter=';')
+    for row in reader:
+        # row=(0-Country; 1-Capital; 2-LatitudeDEG; 3-LongitudeDEG; 4-LatitudeRAD; 5-LongitudeRAD)
+        if row[0] != 'Country':
+            countries.append(row[0:2]+map(float,row[2:]))
+
+# Calculate the distance between countries
+countryNo = len(countries)-6
+print "Country number: ", countryNo
+distance = [[0. for x in range(countryNo)] for y in range(countryNo)]
+k=1
+for i in range(countryNo):
+    for j in range(i+1,countryNo):
+        deltaLong=abs(countries[i][5]-countries[j][5])
+        fi1 = countries[i][4]
+        fi2 = countries[j][4]
+        dist = math.acos(math.sin(fi1)*math.sin(fi2) + math.cos(fi1)*math.cos(fi2)*math.cos(deltaLong))
+        print k, countries[i][1], countries[j][1], dist*R
+        # distance[i][j] = k
+        k+=1
