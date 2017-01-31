8ef4c3bf41456b1d10db46779495fd12ddcb19a5
daniel90 <daniel.pecak@gmail.com>
2017-01-31 23:21:14 +0100

    Inicjalizacja kilku heurystycznych rozwiązań

--- distance.py
+++ distance.py
@@ -2,10 +2,17 @@
 import math
 import random
 import csv
+import sys
+import copy
 
 # Constants:
 R = 6371. # mean radius of the Earth that minimalizes the fact that the Earth
           # is not spherical but elipsoidal
+inf = 10000000.
+
+# System parameters:
+popNo = 10
+heurNo = 4
 
 # Load the CSV file with Countries and coordinates
 countries = []
@@ -19,7 +26,7 @@ with open('south-america.csv','rb') as file:
 # Calculate the distance between countries
 countryNo = len(countries)-6
 print "Country number: ", countryNo
-distance = [[0. for x in range(countryNo)] for y in range(countryNo)]
+distance = [[inf for x in range(countryNo)] for y in range(countryNo)]
 k=1
 for i in range(countryNo):
     for j in range(i+1,countryNo):
@@ -29,11 +36,11 @@ for i in range(countryNo):
         dist = math.acos(math.sin(fi1)*math.sin(fi2) + math.cos(fi1)*math.cos(fi2)*math.cos(deltaLong))
         # print k, countries[i][1], countries[j][1], dist*R
         distance[i][j] = dist*R
+        distance[j][i] = dist*R
         k+=1
 
 
 ### Set population
-popNo = 10
 population = []
 for i in range(popNo):
     basicPerm = range(countryNo)
@@ -43,10 +50,36 @@ for i in range(popNo):
 
 
 ### Set heuristic individuals
-# Pick one guy
-# Get closest neighbour
-# Get closest neighbour of the closes neighbour
-# ...
+heuristicPop = []
+for ii in range(heurNo):
+    # Copy distance list
+    tempdist = copy.deepcopy(distance)
+    # Pick one guy
+    # print ii,"################## Runda 1"
+    basicPerm = range(countryNo)
+    heur = []
+    # print basicPerm
+    luckyGuy = random.sample(basicPerm,1)[0]
+    # print luckyGuy
+    basicPerm.remove(luckyGuy)
+    heur.append(luckyGuy)
+    # print basicPerm
+    # Get closest neighbour
+    for i in range(countryNo-1):
+        # print ii,"################## Runda", i+2
+        # print tempdist[luckyGuy][:]
+        minval = min(tempdist[luckyGuy][:])
+        # print minval
+        oldGuy = luckyGuy
+        luckyGuy = tempdist[luckyGuy].index(minval)
+        # print luckyGuy
+        basicPerm.remove(luckyGuy)
+        heur.append(luckyGuy)
+        # print basicPerm, heur
+        for jj in range(countryNo):
+            tempdist[oldGuy][jj] = inf
+            tempdist[jj][oldGuy] = inf
+    heuristicPop.append(heur)
 ### Fitness function
 ### Choose parents
 ### Davis' Crossover (O1)
