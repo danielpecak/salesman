cb9eb939e30ff27095d4cd9ce0f4c06aee129a4e
daniel90 <daniel.pecak@gmail.com>
2017-02-01 21:10:21 +0100

    Cleaning and planning

--- distance.py
+++ distance.py
@@ -4,28 +4,48 @@ import genetics
 import cities
 # General modules
 import sys
+import random
 
 
 # System parameters:
-popNo = 10
-heurNo = 5
+time   = 100   # number of generations
+popNo  = 10**4 # population size
+heurNo = 5     # number of heruristic solutions
+xmen   = 0.001 # probabilty of mutation
 
 # Load the CSV file with Countries and coordinates
 countries = cities.loadCountries('SA') # South America
 
 # Calculate the distance between countries
 countryNo = len(countries)
-countryNo = 6 # to make everything faster
-print "Country number: ", countryNo
-
 distance = cities.calcDistances(countries)
-distance = [[distance[x][y] for x in range(countryNo)] for y in range(countryNo)] # to make everything faster
 
 ### Set population & heuristics
 population = genetics.growPopulation(popNo,countryNo)
 heuristicPop = genetics.getHeuristicSolutions(distance,countryNo,heurNo)
+print "Population: ", len(population)
+print "Chromosome length: ", len(population[0])
 
-### Brute-force test
-bruteList = genetics.bruteForcePopulation(countryNo)
-print "MIN: ", min([genetics.fitness(i,distance) for i in bruteList])
-print "MAX: ", max([genetics.fitness(i,distance) for i in bruteList])
+### Fitness Based Selection
+for t in range(time):
+    pass
+    # Calculate fitness
+    # Sort population based on fitnesses
+    nextgeneration = []
+    for tt in range(popNo/2):
+        pass
+        # # Pick parents
+        # p1 = genetics.rouletteWheelSelection(population)
+        # p2 = genetics.rouletteWheelSelection(population)
+        # # Breed chromosomes using Crossover
+        # ch1, ch2 = genetics.CrossoverOX1(p1,p2)
+        # # Apply Mutaton with some probability 'xmen'
+        # x = random.random()
+        # if x < xmen: ch1 = genetics.ScrambleMutation(ch1)
+        # x = random.random()
+        # if x < xmen: ch1 = genetics.InversionMutation(ch1)
+        # x = random.random()
+        # if x < xmen: ch2 = genetics.ScrambleMutation(ch2)
+        # x = random.random()
+        # if x < xmen: ch2 = genetics.InversionMutation(ch2)
+        # nextgeneration.append(ch1,ch2)
--- genetics.py
+++ genetics.py
@@ -12,19 +12,19 @@ inf = 10000000.
 ######      GENOM SCALE      ######
 ###################################
 ### MUTATION
-def ScrambleMutation(item2):
+def ScrambleMutation(item):
     "Mutation that shuffles randomly the genes."
-    item=copy.deepcopy(item2)
+    item=copy.deepcopy(item)
+    countryNo = len(item)
     [start,end] = sorted(random.sample(range(countryNo+1),2))
-    print start, end
     shuffle_slice(item,start,end)
     return item
 
-def InversionMutation(item2):
-    item=copy.deepcopy(item2)
+def InversionMutation(item):
+    item=copy.deepcopy(item)
     "Mutation that inverses the gene sequence."
+    countryNo = len(item)
     [start,end] = sorted(random.sample(range(countryNo+1),2))
-    print start, end
     item[start:end] = reversed(item[start:end])
     return item
 
@@ -36,7 +36,7 @@ def shuffle_slice(a, start, stop):
         i += 1
 
 ### Davis' Order Crossover (O1)
-def OX1(p1,p2):
+def CrossoverOX1(p1,p2):
     countryNo=len(p1)
     "Applies so called Davis' Order Crossover OX1 for permutation based crossovers."
     [start,end] = sorted(random.sample(range(countryNo),2))
