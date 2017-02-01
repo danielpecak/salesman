b22cb82e5e705ef04e7c4d76264b5c50ad45828f
daniel90 <daniel.pecak@gmail.com>
2017-02-01 09:36:36 +0100

    Utworzenie pliku do manipulacji mapami
    Wraz z planem do działania

---
+++ mapLib.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# import math
+
+### Load File with points
+name = 'map'
+print "==== Image '"+name+"' has been loaded"
+
+### Open SVG(/PNG?) Image
+
+
+### Draw a Point
+
+
+### Save Image
+
+
+### Draw a Line
+
+
+### Draw an Arrow
+
+
+### Write on an Image
+
+
+### Cut a rectangle from the Image
