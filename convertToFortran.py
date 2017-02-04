dfbde4ee93ff2c2701a01f1e77942d4a68d15bf2
daniel90 <daniel.pecak@gmail.com>
2017-02-04 22:33:19 +0100

    Generated data for Fortran

---
+++ convertToFortran.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import cities
+import sys
+
+codes=['SA','NA','AS','AO','AF','EU','ALL']
+names = ['southAmerica','northAmerica','asia','oceania','africa','europa','world']
+
+for ii in range(len(codes)):
+    code = codes[ii]
+    name = names[ii]
+    country = cities.loadCountries(code)
+    f = open('fortran/{}.f'.format(code.lower()),'w')
+    for c in range(len(country)):
+        string = "{}(:,{:d}) = (/ {:.10f}d0, {:.10f}d0 /)\n".format(name,c+1,country[c][4],country[c][5])
+        f.write(string)
+    f.close()
