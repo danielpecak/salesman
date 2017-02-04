8da39d1e99d1217478083f487f902833b1376067
daniel90 <daniel.pecak@gmail.com>
2017-02-04 01:09:28 +0100

    Added ScrambleMutation & Pick Random Item

--- fortran/genetics.f95
+++ fortran/genetics.f95
@@ -16,5 +16,28 @@ subroutine Shuffle(a)
   end do
 end subroutine Shuffle
 
+function pick_random(a) result(i)
+  ! Slightly modified version of:
+  ! http://rosettacode.org/wiki/Pick_random_element#Fortran
+  integer, intent(in) :: a(:)
+  integer :: i
+  real*8  :: r
+
+  call random_number(r)
+  i = a(int(r*size(a)) + 1)
+end function
+
+! ###################################
+! ######      GENOM SCALE      ######
+! ###################################
+! ### MUTATION
+subroutine ScrambleMutation(item)
+! Mutation that shuffles randomly the genes.
+  integer, intent(inout) :: item(:)
+  integer :: len
+  len = size(item,1)
+
+
+end subroutine ScrambleMutation
 
 end module genetics
