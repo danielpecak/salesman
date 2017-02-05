858f7a32c9bf06ae5b8f7575f731ca3600229c43
daniel90 <daniel.pecak@gmail.com>
2017-02-05 13:32:24 +0100

    Implementing evolution

--- fortran/galgorithm.f95
+++ fortran/galgorithm.f95
@@ -7,11 +7,11 @@ program galgorithm
   real*8  ::  southAmerica(2,13)
   real*8  ::  distances(13,13)
   integer :: ch1(13), ch2(13)
-  type(group), allocatable  :: population(:)
+  type(group), allocatable  :: population(:), temppop(:)
   integer :: time = 1000, popNo = 1000
   real*8  :: xmen = 0.001d0
   real*8  :: meanCase = 55000.d0
-  integer  :: i,j,k,t,p, genNo
+  integer  :: i,j,k,t,p,p1,p2, genNo
   ! Turn on a random number generator
   call random_init_urandom()
   !!! System DEFAULT parameters:
@@ -29,8 +29,11 @@ program galgorithm
   ! TODO
   genNo = size(southAmerica,2)
   popNo = 10
-  allocate(population(popNo))
-  ! allocate(fitnesses(popNo))
+  allocate(temppop(popNo)) ! auxilary population
+  do i=1,popNo
+    call group_create(temppop(i),genNo)
+  enddo!i
+  allocate(population(popNo)) !
   call growPopulation(population,genNo)
 
   !!! Fitness Base Selection
@@ -38,12 +41,26 @@ program galgorithm
   do t=1,time
     if (mod(t,time/10)==0) print *, "Generation #",t,"out of",time
     do p=1,popNo
-      call fitness(population(i),distances,meanCase)
+      call fitness(population(p),distances,meanCase)
     enddo!p
-
     do p=1,popNo/2
+      ! Pick parents
+      call rouletteWheelSelection(population(:)%fitness,p1)
+      call rouletteWheelSelection(population(:)%fitness,p2)
+      ! Breed chromosomes using Crossover
+      call CrossoverOX1(population(p1)%chromosome,population(p2)%chromosome,ch1,ch2)
+      ! Apply mutation with some probability 'XMEN'
+      call random_number(x)
+      if (x<xmen) call ScrambleMutation(temppop(p)%chromosome)
+      call random_number(x)
+      if (x<xmen) call ScrambleMutation(temppop(p+popNo/2)%chromosome)
+      call random_number(x)
+      if (x<xmen) call InversionMutation(temppop(p)%chromosome)
+      call random_number(x)
+      if (x<xmen) call InversionMutation(temppop(p+popNo/2)%chromosome)
       x=xmen
     enddo!p
+    population = temppop
   enddo!t
   !!! Save data
   ! TODO
