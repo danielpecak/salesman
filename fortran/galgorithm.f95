d8fd471b14972654058c51aaa714927c5a48b942
daniel90 <daniel.pecak@gmail.com>
2017-02-05 11:38:35 +0100

    Grouped some variables, QSORT & small fixes

--- fortran/galgorithm.f95
+++ fortran/galgorithm.f95
@@ -4,13 +4,13 @@ program galgorithm
   use slf_random
   implicit none
   real*8  :: x,y
-  real*8  ::  southAmerica(2,12)
-  real*8  ::  distances(12,12)
-  integer, allocatable  :: population(:,:)
-  real*8,  allocatable  :: fitnesses(:)
+  real*8  ::  southAmerica(2,13)
+  real*8  ::  distances(13,13)
+  integer :: ch1(13), ch2(13)
+  type(group), allocatable  :: population(:)
   integer :: time = 1000, popNo = 1000
   real*8  :: xmen = 0.001d0
-  real*8  :: meanCase = 50000.d0
+  real*8  :: meanCase = 55000.d0
   integer  :: i,j,k,t,p, genNo
   ! Turn on a random number generator
   call random_init_urandom()
@@ -22,33 +22,23 @@ program galgorithm
   !!! Load favourite places
   ! TODO
   ! Load SOUTH AMERICA
-  southAmerica(:,1) = (/ -0.6040584541d0,-1.0189232173d0 /)
-  southAmerica(:,2) = (/ -0.3321361567d0,-1.1390018699d0 /)
-  southAmerica(:,3) = (/ -0.275412956d0 ,-0.8365363105d0 /)
-  southAmerica(:,4) = (/ -0.5839871677d0,-1.2330751165d0 /)
-  southAmerica(:,5) = (/  0.0804596785d0,-1.2929399099d0 /)
-  southAmerica(:,6) = (/ -0.0040142573d0,-1.3704325287d0 /)
-  southAmerica(:,7) = (/ -0.9023352233d0,-1.0096729723d0 /)
-  southAmerica(:,8) = (/  0.1186823891d0,-1.015083493d0  /)
-  southAmerica(:,9) = (/ -0.4415683008d0,-1.0060077808d0 /)
-  southAmerica(:,10)= (/ -0.2101376419d0,-1.3444271228d0 /)
-  southAmerica(:,11)= (/  0.1024508271d0,-0.9628981483d0 /)
-  southAmerica(:,12)= (/ -0.6078981785d0,-0.9803514408d0 /)
-  southAmerica(:,12)= (/  0.1830850385d0,-1.1672762037d0 /)
+  include 'data/sa.f'
   call calcDistances(southAmerica,distances)
 
   !!! Set population & heuristics
   ! TODO
   genNo = size(southAmerica,2)
-  allocate(population(genNo,popNo))
-  allocate(fitnesses(popNo))
-  call growPopulation(population)
+  popNo = 10
+  allocate(population(popNo))
+  ! allocate(fitnesses(popNo))
+  call growPopulation(population,genNo)
 
   !!! Fitness Base Selection
   ! TODO
   do t=1,time
+    if (mod(t,time/10)==0) print *, "Generation #",t,"out of",time
     do p=1,popNo
-      call fitness(population(:,p),distances,meanCase,fitnesses(p))
+      call fitness(population(p)%chromosome,distances,meanCase,population(p)%fitness)
     enddo!p
 
     do p=1,popNo/2
--- fortran/genetics.f95
+++ fortran/genetics.f95
@@ -1,7 +1,72 @@
 module genetics
 implicit none
 real*8, parameter :: inf = 1000000000.d0
+
+type group
+  real*8  :: fitness  ! values to be sorted by
+  integer :: order    ! original order of unsorted data
+  integer :: age
+  integer, pointer :: chromosome(:)
+end type group
 contains
+
+! ###################################
+! ####    AUXILIARY FUNCTIONS    ####
+! ###################################
+subroutine group_create(A,genNo)
+  type(group)  :: A
+  integer, intent(in)  :: genNo
+  integer :: i
+  allocate(A%chromosome(1:genNo))
+  A%chromosome = (/ (i, i=1,genNo) /)
+  A%age = 0
+end subroutine group_create
+
+recursive subroutine QSort(A,nA)
+! Type GROUP has been gneralized for purposes of my program, but it's from:
+! http://rosettacode.org/wiki/Sorting_algorithms/Quicksort#Fortran
+  integer, intent(in) :: nA
+  type (group), dimension(nA), intent(in out) :: A
+  ! LOCAL VARIABLES
+  integer :: left, right
+  real*8 :: random
+  real*8 :: pivot
+  type (group) :: temp
+  integer :: marker
+
+      if (nA > 1) then
+          call random_number(random)
+          pivot = A(int(random*real(nA-1))+1)%fitness   ! random pivor (not best performance, but avoids worst-case)
+          left = 0
+          right = nA + 1
+
+          do while (left < right)
+              right = right - 1
+              do while (A(right)%fitness > pivot)
+                  right = right - 1
+              end do
+              left = left + 1
+              do while (A(left)%fitness < pivot)
+                  left = left + 1
+              end do
+              if (left < right) then
+                  temp = A(left)
+                  A(left) = A(right)
+                  A(right) = temp
+              end if
+          end do
+
+          if (left == right) then
+              marker = left + 1
+          else
+              marker = left
+          end if
+
+          call QSort(A(:marker-1),marker-1)
+          call QSort(A(marker:),nA-marker+1)
+      end if
+end subroutine QSort
+
 
 function isElIn(item,list) result(is)
   integer, intent(in) :: list(:), item
@@ -159,16 +224,17 @@ end subroutine cycleLength
 ! ###################################
 ! ######   POPULATION SCALE    ######
 ! ###################################
-subroutine growPopulation(population)
+subroutine growPopulation(population,genNo)
 !  Grow population of P individuals, everyone with G genes.
-  integer, intent(inout) :: population(:,:)
-  integer  :: popNo, genNo
-  integer  :: i, j
-  popNo = size(population,2)
-  genNo = size(population,1)
+  type(group), intent(inout) :: population(:)
+  integer, intent(in)  :: genNo
+  integer  :: popNo
+  integer  :: i
+  popNo = size(population)
   do i=1,popNo
-    population(:,i) = (/ (j, j=1,genNo) /)
-    call shuffle(population(2:genNo,i))
+    call group_create(population(i),genNo)
+    population(i)%order = i
+    call shuffle(population(i)%chromosome(2:genNo))
   enddo!i
 end subroutine growPopulation
 
