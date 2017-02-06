a64b0f8fb9ee9b8251140f71a5bedb2918ad60a7
daniel90 <daniel.pecak@gmail.com>
2017-02-07 00:28:54 +0100

    Write down few examples

--- presentation.py
+++ presentation.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
+
+N=6
+# Original
+ex1 = [1,2,3,4,5,6]
+# Swap mutations
+ex2 = [1,4,3,2,5,6]
+ex3 = [1,5,3,4,2,6]
+# Inversion mutations
+ex4 = [1,5,4,3,2,6]
+ex5 = [1,2,4,3,5,6]
+# Shuffle mutations
+# ex =
+
+ex1 = [1,2,3,4,5,6]
