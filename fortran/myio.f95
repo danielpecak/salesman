3a205141991d20ba98db2cd2962aebce728282e4
daniel90 <daniel.pecak@gmail.com>
2017-02-17 00:35:05 +0100

    Save&Load Snapshots

--- fortran/myio.f95
+++ fortran/myio.f95
@@ -1,34 +1,74 @@
 module myio
+  use genetics
 implicit none
 contains
-subroutine fnameSnapshot(continent,popNo,xmen,t)
+subroutine fnameSnapshot(continent,popNo,xmen,t,output)
   ! Makes the name of the snapshot to save.
-  character(len=*),intent(in) :: continent
+  character(len=*),intent(in)  :: continent
+  character(len=200),intent(out) :: output
   integer, intent(in) :: popNo, t
   real*8, intent(in)  :: xmen
-  character(len=200)  :: var
-  write(var,"(A)") trim(continent)
-  write(var,"(A,A,I6.6)") trim(var), '_P', int(popNo/1000.d0)
-  write(var,"(A,A,F0.6)") trim(var), '_X', xmen*1000.d0
-  write(var,"(A,A,I6.6)") trim(var), '_T', int(t/100.d0)
 
-  ! print "(A,I0.5,f8.4,I0.5)", continent, popNo/1000, xmen*1000.d0, t/100
-  print *, trim(var)
+  write(output,"(A)") trim(continent)
+  write(output,"(A,A,I6.6)") trim(output), '_P', int(popNo/1000.d0)
+  write(output,"(A,A,F0.6)") trim(output), '_X', xmen*1000.d0
+  write(output,"(A,A,I6.6)") trim(output), '_T', int(t/100.d0)
 end subroutine fnameSnapshot
 
-subroutine saveSnapshot()
-  print *, "Hello"
-  continue
+subroutine saveSnapshot(continent,popNo,xmen,t,population,bestpopulation)
+  ! Saves progess during the process.
+  character(len=*),intent(in)  :: continent
+  integer, intent(in)    :: popNo, t
+  real*8,  intent(in)    :: xmen
+  type(group),intent(in) :: population(:), bestpopulation(:)
+  character(len=200)     :: fname, fname2
+  integer :: l, i
+
+  l = size(population(1)%chromosome)
+  call fnameSnapshot(continent,popNo,xmen,t,fname)
+  write(fname2,"(A,A)") 'snapshot/',trim(fname)
+  open(unit=10, status='replace', file=trim(fname2), form='unformatted')
+  write(10) population(1:popNo)%fitness
+  write(10) population(1:popNo)%age
+  do i=1,popNo
+    write(10) population(i)%chromosome(1:l)
+  enddo!i
+  write(10) bestpopulation(1:popNo)%fitness
+  write(10) bestpopulation(1:popNo)%age
+  do i=1,popNo
+    write(10) bestpopulation(i)%chromosome(1:l)
+  enddo!i
+  close(10)
 end subroutine saveSnapshot
 
-subroutine loadSnapshot()
-  print *, "Hello"
-  continue
+subroutine loadSnapshot(continent,popNo,xmen,t,population,bestpopulation)
+  ! Loads saved progess.
+  character(len=*),intent(in)  :: continent
+  integer, intent(in)    :: popNo, t
+  real*8,  intent(in)    :: xmen
+  type(group),intent(out):: population(:), bestpopulation(:)
+  character(len=200)     :: fname, fname2
+  integer :: l, i
+
+  l = size(population(1)%chromosome)
+  call fnameSnapshot(continent,popNo,xmen,t,fname)
+  write(fname2,"(A,A)") 'snapshot/',trim(fname)
+  open(unit=10, status='old', file=trim(fname2), form='unformatted')
+  read(10) population(1:popNo)%fitness
+  read(10) population(1:popNo)%age
+  do i=1,popNo
+    read(10) population(i)%chromosome(1:l)
+  enddo!i
+  read(10) bestpopulation(1:popNo)%fitness
+  read(10) bestpopulation(1:popNo)%age
+  do i=1,popNo
+    read(10) bestpopulation(i)%chromosome(1:l)
+  enddo!i
+  close(10)
 end subroutine loadSnapshot
 
 subroutine lastSnapshot()
   print *, "Hello"
-  continue
 end subroutine lastSnapshot
 
 end module myio
