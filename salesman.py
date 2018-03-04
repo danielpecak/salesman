d2e9942cfe6995d9bfa979e5a0d07d1dae9adf60
daniel90 <daniel.pecak@gmail.com>
2018-03-04 22:05:16 +0100

    Changing the name of the file

--- salesman.py
+++ salesman.py
@@ -28,12 +28,13 @@ popNo  = 10**3  # population size
 heurNo = 5      # number of heruristic solutions
 xmen   = 0.001  # probabilty of mutation
 continent = 'SA'# South America (the least places)
-dsave = 15*10**6 # save every ~30 minutes; 1 mln = 2 minutes
+dsave = 15*10**6 # save every ~30 minutes; 1 mln = 2 minutes (on my laptop)
 if time < 10000:
     dt = time/10
 else:
     dt = time/100
-dsave = 400
+dsave = 4000
+
 ### System optional command line parameter:
 for a in sys.argv:
     if a in ['--population','--pop','-population','-pop']:
@@ -47,7 +48,9 @@ for a in sys.argv:
         continent = sys.argv[sys.argv.index(a)+1]
         if continent.strip() not in allowed:
             sys.exit("""Wrong parameter! \n --type can be one of {}""".format(allowed))
-
+    if a in ['-n','--reset','--new']:
+        myio.newStats(continent,popNo,xmen)
+        myio.clearSnapshots(continent,popNo,xmen,prefix="TEST__")
 
 # Load the CSV file with Countries and coordinates
 countries = cities.loadCountries(continent)
@@ -78,15 +81,15 @@ fitT = []
 varT = []
 for t in range(time0,time0+time):
     if t%dt==0:
-        print "Generation #"+str(t).rjust(4)+" out of "+str(time+time0)+", prob:"+str(xmen).rjust(6)+", population:"+str(popNo).rjust(6)
-    if t%dsave==0:
-        myio.saveSnapshot(continent,popNo,xmen,t,population,bestpopulation)
-        print "Saved"
+        print "Generation #"+str(t).rjust(4)+" out of "+str(time+time0)+" ( prob:"+str(xmen).rjust(6)+", population:"+str(popNo).rjust(6)+")"
+    # if t%dsave==0:
+    #     myio.saveSnapshot(continent,popNo,xmen,t,population,bestpopulation)
+    #     print "Saved"
     # Calculate fitness
     # meanCase = 2*mean([genetics.cycleLength(p,distance) for p in population])
     fitnesses = [genetics.fitness(i,distance,meanCase) for i in population]
     # Sort population based on fitnesses (sort the shit out)
-    fitnesses,population=zip(*sorted(zip(fitnesses,population),reverse=True))
+    fitnesses,population=zip(*sorted(zip(fitnesses,population),reverse=False))
     population = list(population)
     nextgeneration = []
     for tt in range(popNo/2):
@@ -94,6 +97,10 @@ for t in range(time0,time0+time):
         p1 = genetics.rouletteWheelSelection(population,fitnesses)
         p2 = genetics.rouletteWheelSelection(population,fitnesses)
         # Breed chromosomes using Crossover
+        # print tt
+        # print len(population)
+        # print p1
+        # print p1
         ch1, ch2 = genetics.CrossoverOX1(p1,p2)
         # Apply Mutaton with some probability 'xmen'
         x = random.random()
@@ -110,28 +117,39 @@ for t in range(time0,time0+time):
         if x < xmen: ch2 = genetics.SwapMutaton(ch2)
         nextgeneration.append(ch1)
         nextgeneration.append(ch2)
-    bestpopulation = sorted(bestpopulation + population,reverse=True)[0:popNo]
+    # TODO TO NIE JEST POSORTOWANE JAK NALEŻY
+    bestpopulation = sorted(bestpopulation + population,reverse=False)[0:popNo]
     population = nextgeneration
     # Look at the average fitness
     totLength = [genetics.cycleLength(p,distance) for p in population]
     fitT.append(mean(totLength))
     varT.append(math.sqrt(variance(totLength)))
+    # fname = myio.fnameSnapshot(continent,popNo,xmen,time)
+    myio.saveStats(continent,popNo,xmen,t,fitT[-1],varT[-1])
+    # f = open('output/'+fname+'_stats.dat','a')
+    # f.write(" ".join(map(str,[t,fitT[-1],varT[-1]]))+"\n")
+    # f.close()
     # TODO save histogram
 
-### Plot convergence
-plt.plot(range(1+time0,1+time+time0),[x/1000. for x in fitT],label="Mean")
-plt.plot(range(1+time0,1+time+time0),[x/1000. for x in varT],label="sigma")
-plt.ylabel('Path length [km]')
-minx = (meanCase-max(fitT))/1000-1
-maxx = (meanCase-min(fitT))/1000+1
-# TODO Save this data fitT
-# TODO For longer times save just every n-th (n=10 etc.) realization parametrs.
-plt.xlabel('Generation #')
-plt.legend()
+myio.saveSnapshot(continent,popNo,xmen,t,population,bestpopulation,prefix='TEST__')
 
-filename = myio.fnameSnapshot(continent,popNo,xmen,time)
-# filename = '{}time{:0.2f}_Pop{:0.2f}_prob{:0.2f}'.format(continent,math.log10(time),math.log10(popNo), math.log10(xmen))
-plt.savefig('images/{:s}.png'.format(filename))
-print 'Saved in images/{:s}.png'.format(filename)
-# for i in range(5):
-#     mapLib.drawMap(population[i],continent,countries,fname=filename[:-4]+'MAP{}'.format(i+1))
+for p in population:
+    print genetics.cycleLength(p,distance)
+
+# ### Plot convergence
+# plt.plot(range(1+time0,1+time+time0),[x/1000. for x in fitT],label="Mean")
+# plt.plot(range(1+time0,1+time+time0),[x/1000. for x in varT],label="sigma")
+# plt.ylabel('Path length [km]')
+# minx = (meanCase-max(fitT))/1000-1
+# maxx = (meanCase-min(fitT))/1000+1
+# # TODO Save this data fitT
+# # TODO For longer times save just every n-th (n=10 etc.) realization parametrs.
+# plt.xlabel('Generation #')
+# plt.legend()
+#
+# filename = myio.fnameSnapshot(continent,popNo,xmen,time)
+# # filename = '{}time{:0.2f}_Pop{:0.2f}_prob{:0.2f}'.format(continent,math.log10(time),math.log10(popNo), math.log10(xmen))
+# plt.savefig('images/{:s}.png'.format(filename))
+# print 'Saved in images/{:s}.png'.format(filename)
+# # for i in range(5):
+# #     mapLib.drawMap(population[i],continent,countries,fname=filename[:-4]+'MAP{}'.format(i+1))
